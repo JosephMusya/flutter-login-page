47feca9a9bda04bfcab9068d9c9ce64ec6ad2693
JosephMusya <josephmusya254@gmail.com>
2022-08-18 21:21:45 +0300

    Edited the login page disp, title

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -13,140 +13,144 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      // mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Stack(
-          children: [
-            Opacity(
-              opacity: 0.5,
-              child: ClipPath(
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Opacity(
+                opacity: 0.5,
+                child: ClipPath(
+                  clipper: CustomClipperPath(),
+                  child: Container(
+                      width: double.infinity,
+                      height: 340,
+                      decoration:
+                          const BoxDecoration(color: Colors.blueAccent)),
+                ),
+              ),
+              ClipPath(
                 clipper: CustomClipperPath(),
                 child: Container(
-                    width: double.infinity,
-                    height: 340,
-                    decoration: const BoxDecoration(color: Colors.blueAccent)),
-              ),
-            ),
-            ClipPath(
-              clipper: CustomClipperPath(),
-              child: Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.black,
-                  ),
-                  width: double.infinity,
-                  height: 330,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 100, 0, 10),
-                    child: Column(
-                      children: [
-                        const Text(
-                          "Innovators' Club",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                        Opacity(
-                          opacity: 0.5,
-                          child: SizedBox(
-                              height: 120,
-                              child: Image.asset(
-                                'assets/cover/logo.png',
-                                fit: BoxFit.contain,
-                              )),
-                        )
-                      ],
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
                     ),
-                  )),
-            ),
-          ],
-        ),
-        const SizedBox(height: 50),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.blueAccent,
-              borderRadius: BorderRadius.circular(15),
-            ),
-            child: TextFormField(
-              autofocus: false,
-              controller: _emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.mail,
-                    color: Colors.white,
-                  ),
-                  hintText: 'Email Adress',
-                  border: InputBorder.none),
-            ),
+                    width: double.infinity,
+                    height: 330,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 100, 0, 10),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "EBooks",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+                          Opacity(
+                            opacity: 0.5,
+                            child: SizedBox(
+                                height: 120,
+                                child: Image.asset(
+                                  'assets/cover/logo.png',
+                                  fit: BoxFit.contain,
+                                )),
+                          )
+                        ],
+                      ),
+                    )),
+              ),
+            ],
           ),
-        ),
-        const SizedBox(),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            decoration: BoxDecoration(
+          const SizedBox(height: 50),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
                 borderRadius: BorderRadius.circular(15),
-                color: Colors.blueAccent),
-            child: TextFormField(
-              obscureText: true,
-              controller: _passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: const InputDecoration(
-                suffixIcon: Icon(Icons.remove_red_eye, color: Colors.white),
-                prefixIcon: Icon(Icons.key, color: Colors.white),
-                border: InputBorder.none,
-                hintText: 'Password',
+              ),
+              child: TextFormField(
+                autofocus: false,
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.mail,
+                      color: Colors.white,
+                    ),
+                    hintText: 'Email Adress',
+                    border: InputBorder.none),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(18),
-          child: Material(
-            color: Colors.white,
-            elevation: 5,
-            borderRadius: BorderRadius.circular(35),
-            child: MaterialButton(
-              padding: const EdgeInsets.all(10),
-              minWidth: MediaQuery.of(context).size.width * 0.5,
-              onPressed: () {},
-              child: const Text(
-                "Login",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Dont have an account yet?'),
-            const SizedBox(
-              width: 10,
-            ),
-            Material(
-              child: MaterialButton(
-                onPressed: () {},
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
+          const SizedBox(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.blueAccent),
+              child: TextFormField(
+                obscureText: true,
+                controller: _passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.remove_red_eye, color: Colors.white),
+                  prefixIcon: Icon(Icons.key, color: Colors.white),
+                  border: InputBorder.none,
+                  hintText: 'Password',
                 ),
               ),
             ),
-          ],
-        )
-      ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18),
+            child: Material(
+              color: Colors.white,
+              elevation: 5,
+              borderRadius: BorderRadius.circular(35),
+              child: MaterialButton(
+                padding: const EdgeInsets.all(10),
+                minWidth: MediaQuery.of(context).size.width * 0.5,
+                onPressed: () {},
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Dont have an account yet?'),
+              const SizedBox(
+                width: 10,
+              ),
+              Material(
+                child: MaterialButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
