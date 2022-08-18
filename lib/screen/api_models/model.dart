1b72b07458b4f07ba7ab8582f69ac537f27aae0a
JosephMusya <josephmusya254@gmail.com>
2022-08-18 21:21:17 +0300

    Api consumer

---
+++ lib/screen/api_models/model.dart
@@ -0,0 +1,50 @@
+// To parse this JSON data, do
+//
+//     final welcome = welcomeFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Model> modelFromJson(String str) =>
+    List<Model>.from(json.decode(str).map((x) => Model.fromJson(x)));
+
+String modelToJson(List<Model> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Model {
+  int id;
+  String coverImage;
+  String title;
+  String description;
+  String author;
+  int quantity;
+  String status;
+  Model(
+    this.id,
+    this.coverImage,
+    this.title,
+    this.description,
+    this.author,
+    this.quantity,
+    this.status,
+  );
+
+  factory Model.fromJson(Map<String, dynamic> json) => Model(
+        json["id"],
+        json["cover_image"],
+        json["title"],
+        json["description"],
+        json["author"],
+        json["quantity"],
+        json["status"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "cover_image": coverImage,
+        "title": title,
+        "description": description,
+        "author": author,
+        "quantity": quantity,
+        "status": status,
+      };
+}
