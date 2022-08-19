5fce962bfb2e128e6fd066a37ba17f70395973e2
JosephMusya <josephmusya254@gmail.com>
2022-08-19 19:34:55 +0300

    Edited the List type?

--- lib/api_models/api_service.dart
+++ lib/api_models/api_service.dart
@@ -3,17 +3,19 @@ import 'package:innovator/api_models/model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static Future<List<Model>?> getBooks() async {
+  static Future<List<Model>> getBooks() async {
+    List<Model> _model = [];
     try {
       var url = Uri.parse(Api.url + Api.endpoint);
-      var response = await http.get(url);
+      var response = await http.get(url).timeout(const Duration(seconds: 5));
+
       if (response.statusCode == 200) {
-        List<Model> _model = modelFromJson(response.body);
+        _model = modelFromJson(response.body);
         return _model;
       }
     } catch (e) {
       print(e.toString());
     }
-    return null;
+    return _model = [];
   }
 }
