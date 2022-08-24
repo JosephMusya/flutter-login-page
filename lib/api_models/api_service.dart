3acb62b12d549e84fc0dac6083617e32fa4d8391
JosephMusya <josephmusya254@gmail.com>
2022-08-24 21:43:30 +0300

    Added the POST functionality

--- lib/api_models/api_service.dart
+++ lib/api_models/api_service.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:innovator/api_models/api_constants.dart';
 import 'package:innovator/api_models/model.dart';
 import 'package:http/http.dart' as http;
@@ -7,15 +10,26 @@ class ApiService {
     List<Model> _model = [];
     try {
       var url = Uri.parse(Api.url + Api.endpoint);
-      var response = await http.get(url).timeout(const Duration(seconds: 5));
+      var _response = await http.get(url).timeout(const Duration(seconds: 5));
 
-      if (response.statusCode == 200) {
-        _model = modelFromJson(response.body);
+      if (_response.statusCode == 200) {
+        _model = modelFromJson(_response.body);
         return _model;
       }
     } catch (e) {
       print(e.toString());
     }
     return _model = [];
+  }
+
+  static Future addBook(data) async {
+    try {
+      var _response = await http.post(Uri.parse(Api.url),
+          headers: {HttpHeaders.contentTypeHeader: "application/json"},
+          body: jsonEncode(data.toJson(data)));
+      return _response;
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
